0e05de14ea37a21b161643a273c25fde7f1448c6
mysticpillow <batt098@gmail.com>
2023-12-03 22:33:02 +0500

    remove unnecessary `using`

--- TemplateApp/App.axaml.cs
+++ TemplateApp/App.axaml.cs
@@ -1,6 +1,5 @@
 ﻿using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using TemplateApp.ViewModels;
